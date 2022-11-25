645410881e250b5dbd48b0591f221dc2486f9b36
Daniel Furman <djfurman@gmail.com>
2022-11-25 17:51:20 -0500

    Add Chi Router dependnecy. Add Chi Router middleware integrated depenedency. Add Chi CORS dependency. Add a basic mux setup to configure CORS and a router.

---
+++ cmd/api/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// specify permission for who can connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-Amzn-Trace-Id", "X-CSRF-Token"},
+		AllowedMethods:   []string{"DELETE", "GET", "POST", "PUT", "OPTIONS"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		ExposedHeaders:   []string{"Link"},
+		MaxAge:           300,
+	}))
+
+	return mux
+}
