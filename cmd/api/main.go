14c6b98d255be40480fdb264ed003f65dba40c54
Daniel Furman <djfurman@gmail.com>
2022-11-25 18:11:26 -0500

    Add basic DB configuration to the authentication service.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,11 +5,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/djfurman/go-micro-authentication-service/data"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
+const retryDBConnAttempts = 10
+const retryDBConnBackoff = 2 // backoff timer in seconds
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -19,10 +28,17 @@ type Config struct {
 func main() {
 	log.Println("Starting authentication service")
 
-	// ! todo connect to the DB
+	// connect to the DB
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Cannot connect to Postgres")
+	}
 
 	// setup config
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -32,5 +48,45 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	// ! todo identify how to handle a missing DSN
+	dsn := os.Getenv("DSN")
+
+	for {
+
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready...")
+			counts++
+		} else {
+			log.Println("Successfully Connected to Postgres!")
+			return connection
+		}
+
+		if counts > retryDBConnAttempts {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds ...")
+		time.Sleep(retryDBConnBackoff * time.Second)
+		continue
 	}
 }
