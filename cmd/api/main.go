8d4effda2a96262509d3325b86aabf2a75e1ad4f
Daniel Furman <djfurman@gmail.com>
2022-12-08 20:51:12 -0500

    Remove direct DSN variable and build it from enviornment variables.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -66,8 +66,7 @@ func openDB(dsn string) (*sql.DB, error) {
 }
 
 func connectToDB() *sql.DB {
-	// ! todo identify how to handle a missing DSN
-	dsn := os.Getenv("DSN")
+	dsn := fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5", os.Getenv("DB_HOST"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"))
 
 	for {
 
